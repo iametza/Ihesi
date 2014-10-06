8ca34a239a93a324e5b146c8b7072f4f6b612300
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2014-10-06 14:45:21 +0200

    1.4.1 bertsioa argitaratzeko prestatu

---
+++ platforms/android/ant-build/Ihesi-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/home/asier/mnt/weblanak/ihesi-cordova/ihesi/platforms/android/ant-build/Ihesi-release-unsigned.apk : \
+/home/asier/mnt/weblanak/ihesi-cordova/ihesi/platforms/android/ant-build/Ihesi.ap_ \
+/home/asier/mnt/weblanak/ihesi-cordova/ihesi/platforms/android/ant-build/classes.dex \
+/home/asier/mnt/weblanak/ihesi-cordova/ihesi/platforms/android/CordovaLib/ant-build/classes.jar \
